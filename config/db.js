2a6243f29bd50043615447f6250c2bf9a69aacd1
gurisaini <uic.18mca1009@gmail.com>
2023-02-06 23:31:30 +0530

    changes db file

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
-
-mongoose.connect(process.env.DB);
 mongoose.set("strictQuery", true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect(process.env.DB);
+
 const connection = mongoose.connection;
 
 exports.default = connection;
